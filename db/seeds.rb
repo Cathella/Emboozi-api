3a4d9075d1fdf9f6e8f6b4e8c89aa775471e6d60
Cathella <ncathie214@gmail.com>
2021-05-12 07:31:51 +0300

    seed some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Story.create(title: "No woman is perfect.", content: "If you choose a working woman, you have to accept that she can't handle the house full time. If you choose a housewife who can take care of you and manage the home completely, you need to accept that she doesn't make money. If you choose a submissive woman, you must accept that she depends on you. If you choose to be with a brave woman, you must accept that she is tough and has thoughts of her own. If you choose a beautiful woman, you have to accept expenses too. If you choose to be with a great woman, you must accept that she is tough and firm. No woman is perfect. A woman has her own good thing that defines who she is and makes her unique.", slug: "no-woman-is-perfect")
